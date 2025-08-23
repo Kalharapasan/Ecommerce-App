69c6e9f1710ebd1eecf7dce5b496dfc02d923091
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-08-23 09:23:37 +0530

    header component complete

--- ecommerce-app-frontend/src/Components/Header.js
+++ ecommerce-app-frontend/src/Components/Header.js
@@ -40,27 +40,31 @@ const Header = () => {
                     <div className='col-5'>
                         <div className='header-upper-links d-flex align-items-center justify-content-between'>
                             <div>
-                                <Link>
-                                <img src='images/compare.svg' alt='' />
-                                    <p> </p>
+                                <Link className='d-flex align-items-center text-white gap-10'>
+                                <img src='images/compare.svg' alt='compare' />
+                                    <p className='mb-0'>Compre <br /> Product </p>
                                 </Link>
                             </div>
                             <div>
-                                <Link>
-                                <img src=' ' alt='' />
-                                    <p> </p>
+                               <Link className='d-flex align-items-center text-white gap-10'>
+                                <img src='images/wishlist.svg' alt='wishlist' />
+                                    <p className='mb-0'>Favourite <br /> wishlist </p>
                                 </Link>
                             </div>
                             <div>
-                                <Link>
-                                <img src=' ' alt='' />
-                                    <p> </p>
+                                <Link className='d-flex align-items-center text-white gap-10'>
+                                <img src='images/user.svg' alt='user' />
+                                    <p className='mb-0'>Login <br /> My Account </p>
                                 </Link>
                             </div>
                             <div>
-                                <Link>
-                                <img src=' ' alt='' />
-                                    <p> </p>
+                                <Link className='d-flex align-items-center text-white gap-10'>
+                                <img src='images/cart.svg' alt='cart' />
+                                    <div className='d-flex flex-column gap-10'>
+                                        <span className='badge bg-white text-dark'>0</span>
+                                        <p className='mb-0'>500.0$</p>
+
+                                    </div>
                                 </Link>
                             </div>
                         </div>
@@ -68,6 +72,38 @@ const Header = () => {
                 </div>
             </div>
 
+        </header>
+        <header className='header-bottom py-3'>
+            <div className='container-xxl'>
+                <div className='row'>
+                   <div className='col-12'>
+                    <div className='menu-bottom d-flex align-items-center gap-30'>
+
+                        <div>
+                            <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle gap-15  d-flex align-item-center bg-transparent border-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src='images/menu.svg' alt='' /><span className='me-5 d-inline-block'>shop Categories</span>
+                                </button>
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><Link className="dropdown-item text-white" to="">Action</Link></li>
+                                    <li><Link className="dropdown-item text-white" to="">Another action</Link></li>
+                                    <li><Link className="dropdown-item text-white" to="">Something else here</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='menu-links'>
+                            <div className='d-flex align-items-center gap-15'>
+                                <NavLink className='text-white' to="/" >Home</NavLink>
+                                <NavLink className='text-white' to="/" >Our Store</NavLink>
+                                <NavLink className='text-white' to="/" >Blogs</NavLink>
+                                <NavLink className='text-white' to="/" >Contact</NavLink>
+                            </div>
+                        </div>
+
+                    </div>
+                   </div>
+                </div>
+            </div>
         </header>
 
         </>
