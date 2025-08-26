976e363ca3f35327f2ab3eeda34af51d9be30f9a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-08-26 14:20:18 +0530

    update footer component

--- ecommerce-app-frontend/src/Components/Footer.js
+++ ecommerce-app-frontend/src/Components/Footer.js
@@ -16,12 +16,24 @@ const Footer = () => {
 							</div>
 						</div>
 						<div className='col-7'>
-
+								<div className="input-group">
+									<input type="text" className="form-control py-2" 
+									placeholder="You Email Adders" 
+									aria-label="You Email Adders" 
+									aria-describedby="basic-addon2" />
+									<span className="input-group-text p-3" id="basic-addon2">Subscribe</span>
+									</div>
 						</div>
 					</div>
 				</div>
 			</footer>
-			<footer className='py-3'></footer>
+			<footer className='py-3'>
+				<div className='container-xxl'>
+					<div className='row'>
+						
+					</div>
+				</div>
+			</footer>
 			<footer className='py-4'>
 				<div className='container-xxl'>'
 					<div className='row'>
