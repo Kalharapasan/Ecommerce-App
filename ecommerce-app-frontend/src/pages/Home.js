642b84e3efad92b50fce42c3921dda3e7ebd9f3a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-08-26 23:34:31 +0530

    Create Home Page

--- ecommerce-app-frontend/src/pages/Home.js
+++ ecommerce-app-frontend/src/pages/Home.js
@@ -1,11 +1,72 @@
 
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
 	return (
-		<div>
-			Home 
-		</div>
+		<>
+			<section className='home-wrapper-1 py-5'>
+				<div className='container-xxl'>
+					
+					<div className='row'>
+						
+						<div className='col-6'>
+							<div className='main-banner-content position-relative p-3'>
+								<img src='images/main-banner-1.jpg' 
+								className="img-fluid rounded-3" 
+								alt='main banner'/>
+								<div className='main-banner-content position-absolute '>
+									<h4>SUPERCHARGED FOR PROS.</h4>
+									<h5>iPad S13+ Pro.</h5>
+									<p>From $999.00 or $41.62/mo.</p>
+									<Link className='button'>BUY NOW</Link>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-6'>
+							<div className='d-flex flex-wrap gap-10 justify-content-between align-items-center'>
+								<div className='small-banner-content position-relative p-3'>
+									<img src='images/catbanner-01.jpg' 
+									className="img-fluid rounded-3" 
+									alt='main banner'/>
+									<div className='small-banner-content position-absolute '>
+										<h4>SUPERCHARGED FOR PROS.</h4>
+										<h5>iPad S13+ Pro.</h5>
+										<p>From $999.00 or $41.62/mo.</p>
+									</div>
+								</div>
+
+								<div className='small-banner-content position-relative p-3'>
+									<img src='images/catbanner-02.jpg' 
+									className="img-fluid rounded-3" 
+									alt='main banner'/>
+									<div className='small-banner-content position-absolute '>
+										<h4>SUPERCHARGED FOR PROS.</h4>
+										<h5>iPad S13+ Pro.</h5>
+										<p>From $999.00 or $41.62/mo.</p>
+									</div>
+								</div>
+
+								<div className='small-banner-content position-relative p-3'>
+									<img src='images/catbanner-03.jpg' 
+									className="img-fluid rounded-3" 
+									alt='main banner'/>
+									<div className='small-banner-content position-absolute '>
+										<h4>SUPERCHARGED FOR PROS.</h4>
+										<h5>iPad S13+ Pro.</h5>
+										<p>From $999.00 or $41.62/mo.</p>
+									</div>
+								</div>
+
+								
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+		</>
 	);
 };
 
