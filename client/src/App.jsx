438617c45667f659ac9ac096bb1bfd773efbb799
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-14 13:54:18 +0530

    Create Home Page And MainBanner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
