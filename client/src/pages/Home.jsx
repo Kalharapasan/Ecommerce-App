a958fad64064ff5bd05e51aa2f7e3683bd2bb765
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-28 13:10:33 +0530

    Complete Categories part

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../context/MainBanner'
+import Categories from '../context/Categories'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner />
+        <Categories />
     </div>
     
   )
